66dc1458b510f05e729968420d862a9616ae2f3d
adnaanmugis <144472475+adnaanmugis@users.noreply.github.com>
2026-07-12 08:36:47 +0530

    Add files via upload

---
+++ server/models/Note.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema({
+
+    title: String,
+
+    subject: String,
+
+    description: String,
+
+    file: String,
+
+    uploadedBy: String,
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model("Note", noteSchema);
